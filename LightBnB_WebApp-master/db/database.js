d77d0d2ae5ba39fb2439c4f586475aa9d09e25ab
caro-turnbull <caro.turnbull@gmail.com>
2023-08-23 19:02:59 -0400

    refactored getAllProperties to use the db

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,5 +1,15 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
+
 
 /// Users
 
@@ -59,13 +69,22 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = function (options, limit = 10)  {
+
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1;`, [limit])
+    .then((response) => {
+      console.log(response.rows)
+      return response.rows;
+    })
+    .catch((err) => {console.log(err.message)})
 };
+//   const limitedProperties = {};
+//   for (let i = 1; i <= limit; i++) {
+//     limitedProperties[i] = properties[i];
+//   }
+//   return Promise.resolve(limitedProperties);
+// };
 
 /**
  * Add a property to the database
